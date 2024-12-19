25ab879d81f928d0fde42743263ae0eacef5f7a4
a7ro5amurai <m.shuhait@outlook.de>
2024-12-19 14:03:11 +0100

    Typescript_Intro

---
+++ vite-project/src/main.ts
@@ -0,0 +1,113 @@
+// const zahl:number = 5
+
+// console.log(zahl);
+
+const satz: string = "Hallo";
+
+console.log(satz);
+
+let dogName: string = "Leon";
+
+console.log(dogName);
+
+dogName += 11;
+// -> keine Fehlermeldung. Aufgabe konnte nicht wie angegeben erledigt werden.
+
+console.log(dogName);
+
+let wahrheit: boolean = true;
+let Name: string = "Patrick";
+let Number: number = 26;
+
+let seventeen = 0;
+
+const sum = seventeen + 44;
+console.log(sum);
+
+let calcAdditon: number = 1 + 1;
+let addition: string = "addition: "
+console.log(addition + calcAdditon);
+
+// -> oder
+
+console.log("addition: " + calcAdditon);
+
+let calcSubtraction: number = 2 - 1;
+console.log("subtraction: " + calcSubtraction);
+
+let calcMultiplication: number = 2 * 2;
+console.log("multiplication: " + calcMultiplication);
+
+let calcDivision: number = 4 / 2;
+console.log("division: " + calcDivision);
+
+let calcModulus: number = 10 % 6;
+console.log("modulus: " + calcModulus);
+
+let x: number = 20;
+let y: number = 30;
+console.log(x + y);
+
+console.log(y - x);
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+let z: number = 10;
+console.log((x * y) / z);
+
+let resultOne: number = (x * y) / z;
+console.log("resultOne: " + resultOne);
+
+let a: number = 15;
+let b: number = 9;
+
+console.log(a % b);
+
+let c: number = 20;
+
+let resultTwo: number = (a + b) * c;
+console.log("resultTwo: " + resultTwo);
+
+++a
+console.log(a);
+
+--b
+console.log(b);
+
+let resultThree: number = a - b;
+console.log("resultThree: " + resultThree);
+
+console.log(resultOne % resultTwo);
+
+let score: number = 5 + 5 * 10;
+console.log("Ergebnis: " + score);
+
+score = (5 + 5) * 10;
+console.log("Ergebnis: " + score);
+
+score = 0;
+score = score + 10;
+console.log("Ergebnis: " + score);
+
+++score;
+console.log("Ergebnis: " + score);
+
+let zahl = 1;
+zahl = zahl + 1;
+zahl += 1;
+++zahl;
+
+console.log("icrement: " + zahl);
+
+zahl--;
+
+console.log("decrement: " + zahl);
+
+
+
+
+
+
