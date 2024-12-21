ac63dc1b90f5bef8e3d7699e90db9df7ca6fa7d6
a7ro5amurai <m.shuhait@outlook.de>
2024-12-21 11:47:33 +0100

    Typescript-02_numbers

---
+++ vite-project/src/02_numbers/script.ts
@@ -0,0 +1,69 @@
+let test =  15.16698
+console.log(test);
+let a = test.toString()
+console.log(a);
+let b = test.toString(2)
+console.log(b);
+let c = test.toString(8)
+console.log(c);
+let d = test.toString(16)
+console.log(d);
+console.log(test.toFixed(2));
+
+let test2 = 7.78714
+let e = test2.toString()
+console.log(e);
+let f = test2.toString(2)
+console.log(f);
+let g = test2.toString(8)
+console.log(g);
+let h = test2.toString(16)
+console.log(h);
+console.log(test2.toFixed(2));
+
+let test3 = "12.3"
+let i = test3.toString()
+console.log(i);
+let j = test3.toString(2)
+console.log(j);
+let k = test3.toString(8)
+console.log(k);
+let l = test3.toString(16)
+console.log(l);
+console.log(Number(test3).toFixed(2));
+
+let test4 = "3.14random"
+let m = test4.toString()
+console.log(m);
+let n = test4.toString(2)
+console.log(n);
+let o = test4.toString(8)
+console.log(o);
+let p = test4.toString(16)
+console.log(p);
+console.log(Number(test4).toFixed(2));
+
+let test5 = "32px"
+let q = test5.toString()
+console.log(q);
+let r = test5.toString(2)
+console.log(r);
+let s = test5.toString(8)
+console.log(s);
+let t = test5.toString(16)
+console.log(t);
+
+let test6 = true
+console.log(test6);
+let u = test6.toString()
+console.log(u);
+let v = test6.toString(2)
+console.log(v);
+let w = test6.toString(8)
+console.log(w);
+let x = test6.toString(16)
+console.log(x);
+console.log(Number(test6).toFixed(2));
+
+let test7 = "Supercode"
+console.log(Number(test7).toFixed(2));
